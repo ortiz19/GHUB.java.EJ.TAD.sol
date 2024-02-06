54652af695cc4d2ba8d8ea7a301b80fa3080f804
VICTOR ORTIZ LOPEZ <135753640+ortiz19@users.noreply.github.com>
2024-02-06 15:51:08 +0000

    actualizacion de comentarios

--- miPrincipal/AppConjunto.java
+++ miPrincipal/AppConjunto.java
@@ -1,57 +1,73 @@
 package miPrincipal;
-import java.util.Scanner;
-public class AppConjunto{
-    public static void  menu() throws Exception {
-		System.out.println("==================================");
-		System.out.println("    Operaciones con Conjuntos     ");
-        System.out.println("==================================");
-        Conjunto A = new Conjunto();
-		if(A.esVacio())
-			System.out.println("El conjunto A esta vacio");
-		else
-			System.out.println("El conjunto A NO esta vacio");
-		System.out.println("El conjunto A tiene  "+
-			A.cardinal() +" elementos ");
-		//Añadir elementos al conjunto
-		Integer ele = 4;
-		A.añadir(ele);
-		ele = 7;
-		A.añadir(ele);
-		ele = 2;
-		A.añadir(ele);
-		ele = 5;
-		A.añadir(ele);
-		System.out.println("El conjunto A tiene  "+
-				A.cardinal() +" elementos ");
-		//Crear otro conjunto
-		Conjunto B = new Conjunto();
-		//Añadir elementos al conjunto B
-		ele = 6;
-		B.añadir(ele);
-		ele = 3;
-		B.añadir(ele);
-		System.out.println("El conjunto B tiene  "+
-				B.cardinal() +" elementos ");
-		A.union(B);
-		ele = 6;
-		B.retirar(ele);
-		System.out.println("El conjunto B tiene  "+
-				B.cardinal() +" elementos ");
-		B.retirar(ele);
-		//Preguntar si el el 3 pertenece al conjunto A
-		ele = 3;
-		if(A.pertenece(ele))
-			System.out.println("El elemento "+ele+" pertenece a A");
-		else
-			System.out.println("El elemento "+ele+" NO pertenece a A");
-		if(B.pertenece(ele))
-			System.out.println("El elemento "+ele+" pertenece a B");
-		else
-			System.out.println("El elemento "+ele+" NO pertenece a B");
-		
-		System.out.println(A.elemento(2));
-		System.out.println(A.elemento(4));
-         
-    }
 
+import java.util.Scanner;
+
+public class AppConjunto {
+    
+    public static void menu() throws Exception {
+        // Se imprime el encabezado del menú de operaciones con conjuntos
+        System.out.println("==================================");
+        System.out.println("    Operaciones con Conjuntos     ");
+        System.out.println("==================================");
+        
+        // Se crea un nuevo conjunto A
+        Conjunto A = new Conjunto();
+        
+        // Se verifica si el conjunto A está vacío y se imprime el resultado
+        if (A.esVacio())
+            System.out.println("El conjunto A está vacío");
+        else
+            System.out.println("El conjunto A NO está vacío");
+        
+        // Se imprime la cantidad de elementos en el conjunto A
+        System.out.println("El conjunto A tiene " + A.cardinal() + " elementos");
+        
+        // Se añaden elementos al conjunto A
+        Integer ele = 4;
+        A.añadir(ele);
+        ele = 7;
+        A.añadir(ele);
+        ele = 2;
+        A.añadir(ele);
+        ele = 5;
+        A.añadir(ele);
+        
+        // Se imprime la cantidad de elementos en el conjunto A después de añadir elementos
+        System.out.println("El conjunto A tiene " + A.cardinal() + " elementos");
+        
+        // Se crea un nuevo conjunto B
+        Conjunto B = new Conjunto();
+        
+        // Se añaden elementos al conjunto B
+        ele = 6;
+        B.añadir(ele);
+        ele = 3;
+        B.añadir(ele);
+        
+        // Se imprime la cantidad de elementos en el conjunto B
+        System.out.println("El conjunto B tiene " + B.cardinal() + " elementos");
+        
+        // Se realiza la unión del conjunto A con el conjunto B y se imprime la cantidad de elementos en el conjunto B
+        A.union(B);
+        ele = 6;
+        B.retirar(ele);
+        System.out.println("El conjunto B tiene " + B.cardinal() + " elementos");
+        
+        // Se retira un elemento del conjunto B y se imprime si el elemento 3 pertenece a los conjuntos A y B
+        B.retirar(ele);
+        ele = 3;
+        if (A.pertenece(ele))
+            System.out.println("El elemento " + ele + " pertenece a A");
+        else
+            System.out.println("El elemento " + ele + " NO pertenece a A");
+        
+        if (B.pertenece(ele))
+            System.out.println("El elemento " + ele + " pertenece a B");
+        else
+            System.out.println("El elemento " + ele + " NO pertenece a B");
+        
+        // Se imprime el elemento en la posición 2 del conjunto A y del conjunto A
+        System.out.println(A.elemento(2));
+        System.out.println(A.elemento(4));
+    }
 }
--- miPrincipal/AppRational.java
+++ miPrincipal/AppRational.java
@@ -1,38 +1,60 @@
 package miPrincipal;
-import java.util.Scanner;
-public class AppRational{
+
+import java.util.*;
+
+public class AppRational {
 
     public static void menu() throws Exception {
+        // Se crea un objeto Scanner llamado consola para leer la entrada del usuario desde la consola
         Scanner consola = new Scanner(System.in);
+        
+        // Se declara un objeto Rational llamado r1 sin argumentos
         Rational r1 = new Rational();
-        int a,b;
+        
+        // Se declaran variables enteras a y b para almacenar el numerador y denominador del primer número racional
+        int a, b;
+        
+        // Se imprime el encabezado del menú de operaciones con números racionales
         System.out.println("==================================");
         System.out.println("Operaciones con Números Racionales");
         System.out.println("==================================");
-
+        
+        // Se solicita al usuario que ingrese el numerador del primer número racional
         System.out.print("Introduce el numerador del Racional 1:");
         a = consola.nextInt();
-        System.out.print("Introduce el denominado del Racional 1:");
+        
+        // Se solicita al usuario que ingrese el denominador del primer número racional
+        System.out.print("Introduce el denominador del Racional 1:");
         b = consola.nextInt();
-
-        r1=new Rational(a,b);
-
+        
+        // Se crea un nuevo objeto Rational con los valores de numerador y denominador proporcionados por el usuario
+        r1 = new Rational(a, b);
+        
+        // Se imprime el primer número racional ingresado por el usuario
         System.out.println(r1);
 
-        
+        // Se solicita al usuario que ingrese el numerador del segundo número racional
         System.out.print("Introduce el numerador del Racional 2:");
         a = consola.nextInt();
-        System.out.print("Introduce el denominado del Racional 2:");
+        
+        // Se solicita al usuario que ingrese el denominador del segundo número racional
+        System.out.print("Introduce el denominador del Racional 2:");
         b = consola.nextInt();
-
-        Rational r2 = new Rational(a,b);
-
+        
+        // Se crea un nuevo objeto Rational con los valores de numerador y denominador proporcionados por el usuario
+        Rational r2 = new Rational(a, b);
+        
+        // Se imprime el segundo número racional ingresado por el usuario
         System.out.println(r2);
-       
-        System.out.println("Suma = "+r1.toString()+"+"+r2.toString()+"="+r1.add(r1,r2));
-        System.out.println("Multiplica = "+r1.toString()+"*"+r2.toString()+"="+r1.mult(r1,r2));
-        System.out.println("¿Son Iguales? ="+r1+"== "+r2+r1.equal(r1,r2)); 
-
+        
+        // Se realiza la suma de los dos números racionales y se imprime el resultado
+        System.out.println("Suma = " + r1.toString() + "+" + r2.toString() + "=" + r1.add(r1, r2));
+        
+        // Se realiza la multiplicación de los dos números racionales y se imprime el resultado
+        System.out.println("Multiplica = " + r1.toString() + "*" + r2.toString() + "=" + r1.mult(r1, r2));
+        
+        // Se verifica si los dos números racionales son iguales y se imprime el resultado
+        System.out.println("¿Son Iguales? =" + r1 + "== " + r2 + r1.equal(r1, r2)); 
     }
 
 }
